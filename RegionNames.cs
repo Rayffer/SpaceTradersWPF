852560bf2ecf4b559617529f7f450edf82476e29
Rayffer <oscar.guillamon@gmail.com>
2023-05-12 00:34:42 +0200

    Add Static types for views and regions

---
+++ RegionNames.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceTradersWPF;
+
+internal static class RegionNames
+{
+    public static string SplashScreenRegion { get; set; } = "SplashScreenRegion";
+    public static string MainMenuRegion { get; set; } = "MainMenuRegion";
+    public static string MainAreaRegion { get; set; } = "MainAreaRegion";
+}
---
+++ ViewNames.cs
@@ -0,0 +1,8 @@
+﻿namespace SpaceTradersWPF;
+
+internal static class ViewNames
+{
+    public static string AgentGreetingView { get; set; } = "AgentGreetingView";
+    public static string AgentSelectionView { get; set; } = "AgentSelectionView";
+    public static string SplashScreenView { get; set; } = "SplashScreenView";
+}
