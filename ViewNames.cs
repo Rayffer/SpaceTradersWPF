54a1172a5905a8d93c680b46371de9689cc388bc
Rayffer <oscar.guillamon@gmail.com>
2023-05-12 00:36:45 +0200

    Add new view

--- ViewNames.cs
+++ ViewNames.cs
@@ -5,4 +5,5 @@ internal static class ViewNames
     public static string AgentGreetingView { get; set; } = "AgentGreetingView";
     public static string AgentSelectionView { get; set; } = "AgentSelectionView";
     public static string SplashScreenView { get; set; } = "SplashScreenView";
+    public static string MainMenuView { get; set; } = "MainMenuView";
 }
