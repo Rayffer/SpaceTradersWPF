f62e8ec1af391d1fe3e33947274dfb50043befab
Rayffer <oscar.guillamon@gmail.com>
2023-05-13 01:15:20 +0200

    Implement some dashboard fields. Add CommunityToolkit.MVVM

---
+++ Converters/WaypointCoordinatesConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+using SpaceTradersWPF.Models;
+
+namespace SpaceTradersWPF.Converters;
+
+internal class WaypointCoordinatesConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is Waypoint waypoint)
+        {
+            return $"X: {waypoint.x}, Y: {waypoint.y}";
+        }
+        return string.Empty;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
--- ViewModels/DashboardViewModel.cs
+++ ViewModels/DashboardViewModel.cs
@@ -1,13 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Windows.Input;
 
+using Prism.Commands;
 using Prism.Mvvm;
+
+using SpaceTradersWPF.Models;
+using SpaceTradersWPF.Services;
 
 namespace SpaceTradersWPF.ViewModels;
 
 internal class DashboardViewModel : BindableBase
 {
+    private readonly ISpaceTradersApi spaceTradersApi;
+
+    private DelegateCommand loadInformationCommand;
+    private Agent agentInformation;
+    private Waypoint headquarters;
+
+    public ICommand LoadInformationCommand => loadInformationCommand ??= new DelegateCommand(LoadInformation);
+
+    public Agent AgentInformation
+    {
+        get => agentInformation;
+        set => SetProperty(ref agentInformation, value);
+    }
+
+    public Waypoint Headquarters
+    {
+        get => headquarters;
+        set => SetProperty(ref headquarters, value);
+    }
+
+    public DashboardViewModel(
+        ISpaceTradersApi spaceTradersApi)
+    {
+        this.spaceTradersApi = spaceTradersApi;
+    }
+
+    private async void LoadInformation()
+    {
+        this.AgentInformation = await spaceTradersApi.GetAgent();
+        this.Headquarters = await spaceTradersApi.GetWaypoint(this.AgentInformation.headquarters);
+    }
 }
