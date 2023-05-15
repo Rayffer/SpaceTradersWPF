95cc07f2e07dc2e0361530bdd8cec7cc41511556
Rayffer <oscar.guillamon@gmail.com>
2023-05-15 23:40:46 +0200

    add fleet ships overview view

---
+++ ViewModels/AgentFleetShipsOverviewViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+using Prism.Commands;
+using Prism.Mvvm;
+
+using SpaceTradersWPF.Models;
+using SpaceTradersWPF.Services;
+
+namespace SpaceTradersWPF.ViewModels;
+
+internal class AgentShipsOverviewViewModel : BindableBase
+{
+    private readonly ISpaceTradersApi spaceTradersApi;
+    private IEnumerable<Ship> ships;
+    private DelegateCommand loadShipsCommand;
+
+    public IEnumerable<Ship> Ships
+    {
+        get => ships;
+        set => SetProperty(ref ships, value);
+    }
+
+    public ICommand LoadShipsCommand => loadShipsCommand ??= new DelegateCommand(async () => await LoadShips());
+
+    public AgentShipsOverviewViewModel(ISpaceTradersApi spaceTradersApi)
+    {
+        this.spaceTradersApi = spaceTradersApi;
+    }
+
+    private async Task LoadShips()
+    {
+        this.Ships = await this.spaceTradersApi.GetShips(1, 20);
+    }
+}
