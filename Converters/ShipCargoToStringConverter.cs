d223dcb24d3df3fa39c41aa681c28245115b01c0
Rayffer <oscar.guillamon@gmail.com>
2023-05-16 21:21:05 +0200

    Ship cargo converter

---
+++ Converters/ShipCargoToStringConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+using SpaceTradersWPF.Models;
+
+namespace SpaceTradersWPF.Converters;
+
+internal class ShipCargoToStringConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is ShipCargo shipCargo)
+        {
+            return $"{shipCargo.Units}/{shipCargo.Capacity} (Max)";
+        }
+        return "";
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
