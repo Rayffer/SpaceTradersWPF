08b91b78cf098520169ecb7bef72815ce40839c4
Rayffer <oscar.guillamon@gmail.com>
2023-05-17 23:16:57 +0200

    Add Selected  ship binding and display

--- ViewModels/AgentFleetShipsOverviewViewModel.cs
+++ ViewModels/AgentFleetShipsOverviewViewModel.cs
@@ -14,7 +14,14 @@ internal class AgentFleetShipsOverviewViewModel : BindableBase
 {
     private readonly ISpaceTradersApi spaceTradersApi;
     private IEnumerable<Ship> ships;
+    private Ship selectedShip;
     private DelegateCommand loadShipsCommand;
+
+    public Ship SelectedShip
+    {
+        get => selectedShip;
+        set => SetProperty(ref selectedShip, value);
+    }
 
     public IEnumerable<Ship> Ships
     {
@@ -32,5 +39,54 @@ internal class AgentFleetShipsOverviewViewModel : BindableBase
     private async Task LoadShips()
     {
         this.Ships = await this.spaceTradersApi.GetShips(1, 20);
+    }
+
+    private DelegateCommand performExtractionCommand;
+    public ICommand PerformExtractionCommand => performExtractionCommand ??= new DelegateCommand(PerformExtraction);
+
+    private void PerformExtraction()
+    {
+    }
+
+    private DelegateCommand performSurveyCommand;
+    public ICommand PerformSurveyCommand => performSurveyCommand ??= new DelegateCommand(PerformSurvey);
+
+    private void PerformSurvey()
+    {
+    }
+
+    private DelegateCommand performWarpCommand;
+    public ICommand PerformWarpCommand => performWarpCommand ??= new DelegateCommand(PerformWarp);
+
+    private void PerformWarp()
+    {
+    }
+
+    private DelegateCommand performOrbitCommand;
+    public ICommand PerformOrbitCommand => performOrbitCommand ??= new DelegateCommand(PerformOrbit);
+
+    private void PerformOrbit()
+    {
+    }
+
+    private DelegateCommand performDockCommand;
+    public ICommand PerformDockCommand => performDockCommand ??= new DelegateCommand(PerformDock);
+
+    private void PerformDock()
+    {
+    }
+
+    private DelegateCommand performNavigateCommand;
+    public ICommand PerformNavigateCommand => performNavigateCommand ??= new DelegateCommand(PerformNavigate);
+
+    private void PerformNavigate()
+    {
+    }
+
+    private DelegateCommand performRefuelCommand;
+    public ICommand PerformRefuelCommand => performRefuelCommand ??= new DelegateCommand(PerformRefuel);
+
+    private void PerformRefuel()
+    {
     }
 }
