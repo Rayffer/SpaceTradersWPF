76056c62d5517c866e50ecb17883b6867e8d9ddc
Rayffer <oscar.guillamon@gmail.com>
2023-05-19 00:48:53 +0200

    Add area for flyout notifications

--- RegionNames.cs
+++ RegionNames.cs
@@ -5,6 +5,7 @@ internal static class RegionNames
     public static string SplashScreenRegion { get; set; } = "SplashScreenRegion";
     public static string MainMenuRegion { get; set; } = "MainMenuRegion";
     public static string MainAreaRegion { get; set; } = "MainAreaRegion";
-    public static string NotificationAreaRegion { get; set; } = "NotificationAreaRegion";
+    public static string ToastNotificationArea { get; set; } = "ToastNotificationArea";
+    public static string FlyoutNotificationArea { get; set; } = "FlyoutNotificationArea";
     public static string DialogAreaRegion { get; set; } = "DialogAreaRegion";
 }
