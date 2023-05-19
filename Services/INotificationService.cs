1695548223350f5a04256d7281ad0afa2a66e31d
Rayffer <oscar.guillamon@gmail.com>
2023-05-20 00:55:47 +0200

    add notification service to manage notifications in-app

---
+++ Services/INotificationService.cs
@@ -0,0 +1,10 @@
+﻿using SpaceTradersWPF.Types;
+
+namespace SpaceTradersWPF.Services;
+
+internal interface INotificationService
+{
+    void ShowToastNotification(string headerMessage, string bodyMessage, NotificationTypes notificationType, bool clearOtherNotifications = false);
+
+    void ShowFlyoutNotification(string headerMessage, string bodyMessage, NotificationTypes notificationType, bool clearOtherNotifications = false);
+}
---
+++ Services/NotificationService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+using Prism.Events;
+using Prism.Regions;
+
+using SpaceTradersWPF.Events.Models;
+using SpaceTradersWPF.Events;
+using SpaceTradersWPF.Types;
+using SpaceTradersWPF.Views;
+
+namespace SpaceTradersWPF.Services;
+
+internal class NotificationService : INotificationService
+{
+    private readonly IRegionManager regionManager;
+    private readonly IEventAggregator eventAggregator;
+
+    public NotificationService(
+        IRegionManager regionManager,
+        IEventAggregator eventAggregator)
+    {
+        this.regionManager = regionManager;
+        this.eventAggregator = eventAggregator;
+    }
+
+    public void ShowFlyoutNotification(string headerMessage, string bodyMessage, NotificationTypes notificationType, bool clearOtherNotifications = false)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ShowToastNotification(string headerMessage, string bodyMessage, NotificationTypes notificationType, bool clearOtherNotifications = false)
+    {
+        if (clearOtherNotifications)
+        {
+            this.regionManager.Regions[RegionNames.ToastNotificationArea].RemoveAll();
+        }
+
+        this.regionManager.RegisterViewWithRegion(RegionNames.ToastNotificationArea, typeof(ToastNotificationView));
+        this.eventAggregator.GetEvent<NotificationEvent>().Publish(new NotificationEventArguments
+        {
+            ToastNotificationHeader = headerMessage,
+            ToastNotificationTypes = NotificationTypes.PositiveFeedback
+        });
+    }
+}
