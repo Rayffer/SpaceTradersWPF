3d46a09dcb06dae9a9d7940dd5fc9d462486151f
Rayffer <oscar.guillamon@gmail.com>
2023-05-21 01:27:10 +0200

    Add ShipNavigationView

---
+++ ViewModels/ShipNavigationViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Prism.Mvvm;
+using Prism.Regions;
+
+using SpaceTradersWPF.Services;
+using SpaceTradersWPF.Views;
+
+namespace SpaceTradersWPF.ViewModels;
+
+internal class ShipNavigationViewModel : BindableBase
+{
+    private bool closeRequested;
+    private readonly ISpaceTradersApi spaceTradersApi;
+    private readonly IRegionManager regionManager;
+
+    public bool CloseRequested
+    {
+        get => this.closeRequested;
+        set => SetProperty(ref this.closeRequested, value);
+    }
+
+    public ShipNavigationViewModel(ISpaceTradersApi spaceTradersApi,
+        IRegionManager regionManager)
+    {
+        this.spaceTradersApi = spaceTradersApi;
+        this.regionManager = regionManager;
+    }
+
+    internal void RemoveElementAnimationCompleted(FlyoutNotificationView view)
+    {
+        if (!this.regionManager.Regions[RegionNames.DialogAreaRegion].Views.Contains(view))
+        {
+            return;
+        }
+
+        this.regionManager.Regions[RegionNames.DialogAreaRegion].Remove(view);
+    }
+}
---
+++ Views/ShipNavigationView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SpaceTradersWPF.Views
+{
+    /// <summary>
+    /// Interaction logic for ShipNavigationView.xaml
+    /// </summary>
+    public partial class ShipNavigationView : UserControl
+    {
+        public ShipNavigationView()
+        {
+            InitializeComponent();
+        }
+
+        private void DoubleAnimation_Completed(object sender, EventArgs e)
+        {
+        }
+    }
+}
