58fcee993ad0c19123d03aa7c60679b85d5b48bb
Rayffer <oscar.guillamon@gmail.com>
2023-05-30 00:06:06 +0200

    Add collection null or empty converters

---
+++ Converters/CollectionIsNullOrEmptyConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace SpaceTradersWPF.Converters;
+
+internal class CollectionIsNullOrEmptyConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return value is null
+               || (value is ICollection collection
+                   && collection.Count == 0) ? Visibility.Visible : Visibility.Collapsed;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
