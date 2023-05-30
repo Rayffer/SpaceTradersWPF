424261f694e8a9adfb4bc41dacd2c3dcbf4c9b46
Rayffer <oscar.guillamon@gmail.com>
2023-05-30 23:54:10 +0200

    Add converters for mainview

---
+++ Converters/BooleanToMinWidthConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace SpaceTradersWPF.Converters;
+
+internal class BooleanToMinWidthConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is bool boolValue && boolValue)
+        {
+            return 400;
+        }
+        return 0;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Converters/ListAndDetailContentToButtonVisibilityConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace SpaceTradersWPF.Converters;
+
+internal class ListAndDetailContentToButtonVisibilityConverter : IMultiValueConverter
+{
+    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (values.Length == 2 &&
+            values[0] is bool listHasContent &&
+            !listHasContent &&
+            values[1] is bool detailHasContent &&
+            detailHasContent)
+        {
+            return Visibility.Visible;
+        }
+        return Visibility.Collapsed;
+    }
+
+    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
