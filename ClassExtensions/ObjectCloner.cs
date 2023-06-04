f6f58adecb4a4f311a8453d74e223b7fcd64ceee
Rayffer <oscar.guillamon@gmail.com>
2023-06-05 00:46:03 +0200

    add method to clone objects

---
+++ ClassExtensions/ObjectCloner.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace SpaceTradersWPF.ClassExtensions;
+
+internal static class ObjectCloner
+{
+    public static T Clone<T>(this T objectToClone) where T : class
+    {
+        var serializedMonster = JsonConvert.SerializeObject(objectToClone, Formatting.None);
+        return JsonConvert.DeserializeObject<T>(serializedMonster);
+    }
+}
