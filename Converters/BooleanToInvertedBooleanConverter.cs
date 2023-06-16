dd70afd9831fc012719873f9ec9136fde1ca4580
Rayffer <oscar.guillamon@gmail.com>
2023-06-16 16:44:01 +0200

    Added converters for controlling visibility with multibindings

---
+++ Converters/BooleanToInvertedBooleanConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace SpaceTradersWPF.Converters;
+
+internal class BooleanToInvertedBooleanConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return value is bool boolValue ? !boolValue : (object)false;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Converters/BooleanToVisibilityMultiConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Windows;
+using System.Windows.Data;
+
+namespace SpaceTradersWPF.Converters
+{
+    internal class BooleanToVisibilityMultiConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return values.Any(x => x is bool booleanValue && booleanValue) ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
