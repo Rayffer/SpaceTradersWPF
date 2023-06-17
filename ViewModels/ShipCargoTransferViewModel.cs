09216a0df840fb114c0ab28d792a63f82572634f
Rayffer <oscar.guillamon@gmail.com>
2023-06-17 13:59:32 +0200

    Add ShipCargo TransferView

--- ViewModels/ShipCargoTransferViewModel.cs
+++ ViewModels/ShipCargoTransferViewModel.cs
@@ -1,28 +1,127 @@
-﻿using Prism.Commands;
-
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-
 using System.Windows.Input;
+
+using Prism.Commands;
+using Prism.Events;
+using Prism.Mvvm;
+using Prism.Regions;
+
+using SpaceTradersWPF.ApiModels;
+using SpaceTradersWPF.Events;
+using SpaceTradersWPF.Events.Models;
+using SpaceTradersWPF.Services;
+using SpaceTradersWPF.Types;
+using SpaceTradersWPF.Views;
 
 namespace SpaceTradersWPF.ViewModels;
 
-internal class ShipCargoTransferViewModel
+internal class ShipCargoTransferViewModel : BindableBase
 {
+    private int cargoAmountToTransfer;
+    private Ship ship;
+    private Ship selectedShipToTransferTo;
+    private Inventory selectedCargo;
+    private IEnumerable<Ship> waypointShips;
+    private readonly IRegionManager regionManager;
+    private readonly ISpaceTradersApi spaceTradersApi;
+    private readonly IEventAggregator eventAggregator;
+    private readonly INotificationService notificationService;
     private DelegateCommand cancelNavigationCommand;
-    private DelegateCommand performCargoTransferCommand;
+    private DelegateCommand<object[]> performCargoTransferCommand;
+    private readonly SubscriptionToken subscriptionToken;
 
-    public ICommand CancelNavigationCommand => cancelNavigationCommand ??= new DelegateCommand(CancelNavigation);
-    public ICommand PerformCargoTransferCommand => performCargoTransferCommand ??= new DelegateCommand<object[]>(PerformCargoTransfer);
+    public ICommand CancelNavigationCommand => this.cancelNavigationCommand ??= new DelegateCommand(this.CancelNavigation);
+    public ICommand PerformCargoTransferCommand => this.performCargoTransferCommand ??= new DelegateCommand<object[]>(async arguments => await this.PerformCargoTransfer(arguments));
+
+    public IEnumerable<Ship> WaypointShips
+    {
+        get => this.waypointShips;
+        set => this.SetProperty(ref this.waypointShips, value);
+    }
+
+    public Ship Ship
+    {
+        get => this.ship;
+        set => this.SetProperty(ref this.ship, value);
+    }
+
+    public int CargoAmountToTransfer
+    {
+        get => this.cargoAmountToTransfer;
+        set => this.SetProperty(ref this.cargoAmountToTransfer, value);
+    }
+
+    public Inventory SelectedCargo
+    {
+        get => this.selectedCargo;
+        set => this.SetProperty(ref this.selectedCargo, value);
+    }
+
+    public Ship SelectedShipToTransferTo
+    {
+        get => this.selectedShipToTransferTo;
+        set => this.SetProperty(ref this.selectedShipToTransferTo, value);
+    }
+
+    public ShipCargoTransferViewModel(
+        IRegionManager regionManager,
+        ISpaceTradersApi spaceTradersApi,
+        IEventAggregator eventAggregator,
+        INotificationService notificationService)
+    {
+        this.regionManager = regionManager;
+        this.spaceTradersApi = spaceTradersApi;
+        this.eventAggregator = eventAggregator;
+        this.notificationService = notificationService;
+        this.subscriptionToken = this.eventAggregator
+            .GetEvent<ShipCargoTransferEvent>()
+            .Subscribe(async Ship => await this.LoadInformation(Ship));
+    }
+
+    private async Task LoadInformation(ShipCargoTransferEventArguments ship)
+    {
+        this.eventAggregator
+            .GetEvent<ShipCargoTransferEvent>()
+            .Unsubscribe(this.subscriptionToken);
+
+        this.Ship = ship.ShipToTransferFrom;
+        var ships = await this.spaceTradersApi.GetShips(1, 20);
+        this.WaypointShips = ships.Where(ship => ship.NavigationInformation.WaypointSymbol == this.Ship.NavigationInformation.WaypointSymbol).ToList();
+    }
 
     private void CancelNavigation()
     {
+        this.RemoveView();
     }
 
-    private void PerformCargoTransfer(object[] arguments)
+    private async Task PerformCargoTransfer(object[] arguments)
     {
+        var cargoTransferResult = await this.spaceTradersApi.PostShipTransferCargo(this.Ship.Symbol, new ApiModels.Requests.PostShipTransferCargoRequest
+        {
+            ShipSymbol = this.SelectedShipToTransferTo.Symbol,
+            Symbol = this.SelectedCargo.Symbol,
+            Units = this.CargoAmountToTransfer
+        });
+
+        this.notificationService
+            .ShowToastNotification(
+                $"Transfered cargo from {this.Ship.Symbol} to {this.SelectedShipToTransferTo.Symbol}",
+                $"Transfered {this.CargoAmountToTransfer} units of {this.SelectedCargo.Symbol}",
+                NotificationTypes.PositiveFeedback);
+
+        this.RemoveView();
+    }
+
+    private void RemoveView()
+    {
+        if (!this.regionManager.Regions[RegionNames.DialogAreaRegion].Views.OfType<ShipNavigationView>().Any())
+        {
+            return;
+        }
+
+        var viewToRemove = this.regionManager.Regions[RegionNames.DialogAreaRegion].Views.OfType<ShipNavigationView>().Single();
+        this.regionManager.Regions[RegionNames.DialogAreaRegion].Remove(viewToRemove);
     }
 }
